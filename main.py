f96777284460454e0898556216812f281228881a
Gabriel-Andrei Neagu <gabrielneagu02@yahoo.com>
2026-05-17 15:25:20 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import sys
+import cv2
+
+from PySide6.QtCore import Qt, QTimer
+from PySide6.QtGui import QImage, QPixmap
+from PySide6.QtWidgets import (
+    QApplication,
+    QFileDialog,
+    QHBoxLayout,
+    QLabel,
+    QMainWindow,
+    QPushButton,
+    QSlider,
+    QVBoxLayout,
+    QWidget,
+    QGroupBox,
+    QFormLayout,
+)
+
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
